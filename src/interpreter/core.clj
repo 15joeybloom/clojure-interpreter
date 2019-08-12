48e568babb21809392650c721fb878c45a407046
Joey Bloom <15joeybloom@gmail.com>
2019-08-11 23:39:47 -0500

    Syntax quote and unquote. Runtime.

--- src/interpreter/core.clj
+++ src/interpreter/core.clj
@@ -48,8 +48,38 @@
 
 (hiccup->lisp (parse-clojure "(+ 12 (inc 6) (if true 7 8))"))
 
-(defn my-eval-list [evalfn env [f & args]]
+(defn my-eval-syntax-quote [evalfn env level exp]
+  (if (list? exp)
+    (let [[f arg] exp]
+      (cond
+        (= f 'unquote)
+        (case level
+          0 (evalfn env arg)
+          (let [[env' result] (my-eval-syntax-quote evalfn env (dec level) arg)]
+            [env' (list 'unquote result)]))
+
+        (= f 'syntax-quote)
+        (let [[env' result] (my-eval-syntax-quote evalfn env (inc level) arg)]
+          [env' (list 'syntax-quote result)])
+
+        :else
+        (let [[env' result-vec]
+              (reduce (fn [[before-env results] sub-exp]
+                        (let [[after-env result]
+                              (my-eval-syntax-quote evalfn env level sub-exp)]
+                          [after-env (conj results result)]))
+                      [env []]
+                      exp)]
+          [env' (apply list result-vec)])))
+    [env exp]))
+
+(defn my-eval-list [evalfn env [f & args :as list-exp]]
   (cond
+    (empty? list-exp) list-exp
+    (= f 'quote) (first args)
+    (= f 'unquote) (throw+ {:type :unquote-not-in-syntax-quote
+                            :unquoted-expression list-exp})
+    (= f 'syntax-quote) (my-eval-syntax-quote evalfn env 0 (first args))
     (= f 'if) (let [[condition then else] args
                     [env' result] (evalfn env condition)]
                 (evalfn env' (if result then else)))
@@ -60,8 +90,14 @@
                     [env'] (evalfn env stmt)]
                 (evalfn env' expr))))
 
+(defmacro construct-syms [& syms]
+  (into {} (for [s syms] [`'~s (list :primitive-fn s)])))
+
+(def runtime (construct-syms cons list first next rest
+                             + - * / mod rem quot))
+
 (defn my-eval
-  ([t] (second (my-eval {} t)))
+  ([t] (second (my-eval runtime t)))
   ([env t]
    (cond
      (contains? #{nil true false} t) [env t]
@@ -93,7 +129,19 @@
 (interpret-clojure "(if false 1 2)")
 (interpret-clojure "(if true 2 3)")
 (interpret-clojure "(let-one x 3 (let-one x 4 x))")
+(interpret-clojure "list")
+(interpret-clojure "(syntax-quote (1 2))")
+(interpret-clojure "(syntax-quote (syntax-quote asdf))")
+(interpret-clojure "(let-one x 3 (syntax-quote (+ (unquote x) 2)))")
+(interpret-clojure "(let-one x 3 (syntax-quote (syntax-quote (+ (unquote (unquote x)) 2))))")
 
+
+;; A good technique to check that environment is plumbed correctly
+(let [y (atom [])]
+  (+ (do (swap! y conj (quote hi)) 1)
+     (do (swap! y conj (quote its)) 2)
+     (do (swap! y conj (quote joey)) 3))
+  @y)
 
 ;; Exercises:
 ;;
